a0ec5ec5bd0926c4dd46104f9b45307cb75b193b
jonathanzm8 <jzhina.done@gmail.com>
2024-01-24 15:37:31 -0500

    calcular el valor total ok

--- puntoDeVenta.js
+++ puntoDeVenta.js
@@ -61,7 +61,10 @@ calcularValorTotal = function(){
             Si el caso de prueba es exitoso, hacer un commit
         */
     //10. Invocar a calcularTotal y lo que devuelve guardar en la variable valorTotal
+    valorTotal= calcularTotal(valorSubtotal,valorDescuento,valorIVA)
     //11. Mostrar el resultado en el componente lblTotal
+    mostrarTexto("lblTotal", valorTotal);  
+
      /*
             Caso de prueba: 
                 precioProducto: 5.4  cantidad: 10 descuento: 10

0b2dde1af80fb53c8b21fa072b7934c2eaadd739
jonathanzm8 <jzhina.done@gmail.com>
2024-01-24 11:17:27 -0500

    funcion mostrar texto implementada

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -1,12 +1,26 @@
 saludar= function(){
     let nombre, apellido;
     let edad, estatura;
+    let mensaje
     // recupero el texto llammado a la funcion recuperarTexto
 
     nombre= recuperarTexto("txtNombre");
     apellido= recuperarTexto("txtApellido");
     edad= recuperarInt("txtEdad");
     estatura= recuperarFloat("txtEstatura");
+
+    mensaje= "BIENVENIDO:"+" "+ nombre +" "+ apellido;
+
+    mostraTExto("lblResultado", mensaje)
+
+
+}
+
+
+mostraTExto= function(idcomponete, mensaje){
+    let componente
+    componente=document.getElementById(idcomponete);
+    componente.innerText=mensaje;
 }
 
 

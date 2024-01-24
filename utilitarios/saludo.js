b029ce58e89a6c49ba8a7b721de51d3f3f3a51cf
jonathanzm8 <jzhina.done@gmail.com>
2024-01-24 10:57:25 -0500

    recuperar entero y float

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -1,9 +1,12 @@
 saludar= function(){
     let nombre, apellido;
+    let edad, estatura;
     // recupero el texto llammado a la funcion recuperarTexto
 
     nombre= recuperarTexto("txtNombre");
     apellido= recuperarTexto("txtApellido");
+    edad= recuperarInt("txtEdad");
+    estatura= recuperarFloat("txtEstatura");
 }
 
 
@@ -13,4 +16,21 @@ recuperarTexto=function(idcomponete){
     componente=document.getElementById(idcomponete);
     valorIngresado=componente.value;
     return valorIngresado;
+}
+
+recuperarInt=function(idcomponete){
+    let valorCaja, valorEntero;
+    // llamo a la funcion recuperarTexto que recupera en componente
+    valorCaja=recuperarTexto(idcomponete);
+    valorEntero=parseInt(valorCaja);
+    return valorEntero
+}
+
+
+recuperarFloat=function(idcomponete){
+    let valorCaja, valorFloat;
+    // llamo a la funcion recuperarTexto que recupera en componente
+    valorCaja=recuperarTexto(idcomponete);
+    valorFloat=parseFloat(valorCaja);
+    return valorFloat
 }

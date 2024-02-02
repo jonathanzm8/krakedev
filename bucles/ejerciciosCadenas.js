0adf8b52d992d5fa06ac03d58c179bf537df4b83
jonathanzm8 <jzhina.done@gmail.com>
2024-02-02 18:54:37 -0500

    reto 38 cadadena invertida esta en la carpeta bucles

---
+++ bucles/ejerciciosCadenas.js
@@ -0,0 +1,69 @@
+ejecutar= function(){
+    let mensaje;
+
+    mensaje=recuperarTexto("txtCadena") ;
+
+    recorrerCAdena(mensaje);
+}
+
+inveritr= function(){
+    let mensaje;
+    let invertido
+
+    mensaje=recuperarTexto("txtCadena") ;
+
+    invertido=invertirCadena(mensaje)
+
+    mostrarTexto("lblInvertido", invertido)
+
+}
+
+
+
+
+
+
+recorrerCAdena= function(cadena){
+
+
+    // cadena= juan
+
+    let caracter;
+    // for(let posicion=0; posicion<cadena.length;posicion++){
+
+    //     caracter= cadena.charAt(posicion);
+
+    //     console.log("el caracter es"+ caracter+ "en la posicion"+ posicion)
+
+    // }
+    //-------------------------------------- diferente forma----------------------------------------------------------
+
+    for(let posicion=0;posicion<=cadena.length-1;posicion++){
+
+        caracter= cadena.charAt(posicion);
+
+        console.log("el caracter es   "+ caracter +" "+ "en la posicion"+ posicion)
+
+    }
+}
+
+invertirCadena= function(cadena){  
+
+    let letra;
+    let resultado ="";
+
+    for(let posicion= cadena.length-1; posicion>=0; posicion--){
+
+        letra= cadena.charAt(posicion); // guarda la letra en la posicion
+
+        resultado= resultado +letra
+
+    }
+
+    return resultado;
+
+
+    
+
+
+}

0b59539bf7e66ca2a292e2d42d9ee8cfca1c5a27
jonathanzm8 <jzhina.done@gmail.com>
2024-02-01 21:59:19 -0500

    reto 37 mostar numeros impares

---
+++ bucles/ejemplosFor.js
@@ -0,0 +1,83 @@
+mostarNumeros= function () {
+
+    console.log("antes del for")
+
+    for(let i=0; i< 4; i++){
+
+        console.log(i)
+
+    }
+
+    console.log("despues del for")
+    
+}
+
+mostarNumeros2= function(){
+    console.log("antes del for")
+
+    for(let indice=1; indice<= 5; indice++){
+
+        console.log(indice)
+
+    }
+
+
+
+    console.log("despues del for")
+
+}
+
+numerosPares= function(){
+
+    console.log("antes del for")
+
+    for(let pares=2; pares<=10; pares+=2){
+
+        console.log(pares)
+
+    }
+
+
+    console.log("despues del for")
+}
+
+mostrarInversos= function(){
+    console.log("antes del for")
+
+    for(let inverso=10; inverso>0; inverso--){
+
+        console.log(inverso)
+
+    }
+
+    console.log("despues del for")
+
+
+
+}
+
+hakearNasa= function(){
+    // hakear nasa de 0% .... 100% de 10 
+
+    for(let nasa=0;nasa<=100; nasa+=10){
+
+        console.log("hakeando nasa"+ nasa+ "%")
+
+    }
+
+    console.log("la nasa ha sido hakeada")
+}
+
+mostarImpares=function(){
+    console.log("antes del for")
+
+    for(let impar=1; impar<=21;impar+=2){
+
+        console.log(impar)
+    }
+
+    console.log("despues del for")
+
+
+
+}

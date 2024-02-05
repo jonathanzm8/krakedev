179146d1623e0f37e2a6a46079e205f210d30573
jonathanzm8 <jzhina.done@gmail.com>
2024-02-05 12:42:26 -0500

    reto 40 evidencia

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,6 +1,9 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
 let palabraSecreta = ""
-let intentos;
+let intentos = 0;
+let coincidencias = 0;
+
+let errores = 0;
 
 guardarPalabra = function () {
 
@@ -13,6 +16,8 @@ guardarPalabra = function () {
         console.log(palabraSecreta)
     } else {
         alert("ingrese una palabra de 5 caracteres y mayusculas")
+
+
     }
 
 
@@ -24,82 +29,158 @@ mostraLetra = function (letra, posicion) {
 
 
 
-        if ( posicion ==  0 ) {
-            mostrarTexto("div0", letra)
-        }
-        if ( posicion ==  1 ) {
-            mostrarTexto("div1", letra)
-        }
-        if ( posicion ==  2 ) {
-            mostrarTexto("div2", letra)
-        }
-        if ( posicion ==  3 ) {
-            mostrarTexto("div3", letra)
-        }
-        if ( posicion ==  4) {
-            mostrarTexto("div4", letra)
-        }
-  
-
-
- }
-
-
-    validar = function (letra) {
-        let letrasEncontradas = 0;
-        for (let i = 0; i < palabraSecreta.length; i++) {
-
-            letrasIngresada = palabraSecreta.charAt(i)
-
-            if (letra == letrasIngresada) {
-               
-                mostraLetra(letra,i)
-                letrasEncontradas++;
-            }
-
-        }
-
+    if (posicion == 0) {
+        mostrarTexto("div0", letra)
+    }
+    if (posicion == 1) {
+        mostrarTexto("div1", letra)
+    }
+    if (posicion == 2) {
+        mostrarTexto("div2", letra)
+    }
+    if (posicion == 3) {
+        mostrarTexto("div3", letra)
+    }
+    if (posicion == 4) {
+        mostrarTexto("div4", letra)
     }
 
 
-    ingresarLetra = function () {
-        let letra
-        letra = recuperarTexto("txtLetra")
 
-        letra.toUpperCase()
+}
 
 
-        if (letra == letra.toUpperCase()) {
-            validar(letra)
+validar = function (letra) {
+    let letrasEncontradas = 0;
+    let coinciden = coincidencias;
 
-        } else {
-            alert("solo se aceptan letras mayusculas")
+    for (let i = 0; i < palabraSecreta.length; i++) {
+
+
+        letrasPosicion = palabraSecreta.charAt(i)
+
+        if (letra == letrasPosicion) {
+            coincidencias++;
+
+            mostraLetra(letra, i)
+            letrasEncontradas++;
         }
 
+        console.log(coinciden)
 
     }
 
 
 
+    if (coinciden == coincidencias) {
+        errores++;
+        mostrarAhorcado();
+
+        alert("LA LETRA NO ES PARTE DE LA PALABRA")
+
+
+    }
 
 
 
 
 
+}
+
+
+ingresarLetra = function () {
+    let letra
+    letra = recuperarTexto("txtLetra")
+
+    letra.toUpperCase()
+    intentos++;
 
 
 
-    esMayuscula = function (caracter) {
+    if (letra == letra.toUpperCase()) {
 
 
-        if (caracter > 64 && caracter < 91) { // entre 65 y 90
+        validar(letra)
+        if (coincidencias == 5) {
+            // alert("HA GANADO")
+            mostrarImagen("ahorcadoImagen", "ganador.gif")
 
-            return true;
+        }
+        if (intentos == 10) {
+            mostrarImagen("ahorcadoImagen", "gameOver.gif")
 
-        } else {
 
-            return false;
+            alert("HA PERDIDO")
 
         }
 
+
+
+    } else {
+        alert("solo se aceptan letras mayusculas")
+
+
+
     }
+
+
+}
+
+
+
+mostrarAhorcado = function () {
+    console.log(errores)
+    if (errores == 1) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_01.png")
+    }
+    if (errores == 2) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_02.png")
+    }
+    if (errores == 3) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_03.png")
+    }
+    if (errores == 4) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_04.png")
+    }
+    if (errores == 5) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_05.png")
+    }
+    if (errores == 6) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_06.png")
+    }
+    if (errores == 7) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_07.png")
+    }
+    if (errores == 8) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_08.png")
+    }
+    if (errores == 9) {
+        mostrarImagen("ahorcadoImagen", "Ahorcado_09.png")
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+esMayuscula = function (caracter) {
+
+
+    if (caracter > 64 && caracter < 91) { // entre 65 y 90
+
+        return true;
+
+    } else {
+
+        return false;
+
+    }
+
+}

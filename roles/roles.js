baa5bb7ffb2be6ce3a6c4cd1800a8de194d095e6
jonathanzm8 <jzhina.done@gmail.com>
2024-02-09 22:42:43 -0500

    reto 46 roles

--- roles/roles.js
+++ roles/roles.js
@@ -1,19 +1,53 @@
 let empleados = [
-    {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
-    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
+    { cedula: "1714616123", nombre: "John", apellido: "Cena", sueldo: 500.0 },
+    { cedula: "0914632123", nombre: "Luisa", apellido: "Gonzalez", sueldo: 900.0 },
+    { cedula: "1109856432", nombre: "Jonathan", apellido: "Morocho", sueldo: 850.0 }
 ]
 
 
-mostraOpcionEmpleado=function(){
+mostarEmpoleados = function () {
+    let cmpTable, contenidoTabla, element;
+
+    cmpTable = document.getElementById("tablaEmpleados")
+
+    contenidoTabla = "<table> <tr>" +
+        "<th>CEDULA</th>"+
+        "<th>NOMBRE</th>"+
+        "<th>APELLIDO</th>"+
+        "<th>SUELDO</th>"+
+        "</tr>"
+
+    for(let i= 0; i< empleados.length; i++){
+        element= empleados[i];
+        contenidoTabla +=
+        "<tr>"+
+        "<td>"+ element.cedula +"</td>" +
+        "<td>"+ element.nombre +"</td>" +
+        "<td>"+ element.apellido +"</td>" +
+        "<td>"+ element.sueldo +"</td>" +
+        "</tr>"
+    }
+
+    contenidoTabla += "</table>"
+    cmpTable.innerHTML = contenidoTabla;
+
+}
+
+
+
+
+mostraOpcionEmpleado = function () {
 
     mostrarComponente("divEmpleado");
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
 
+    mostarEmpoleados();
+
 }
 
 
-mostraOpcioRol= function(){
+mostraOpcioRol = function () {
 
     mostrarComponente("divRol");
     ocultarComponente("divEmpleado");
@@ -21,7 +55,7 @@ mostraOpcioRol= function(){
 
 }
 
-mostraOpcionResumen= function(){
+mostraOpcionResumen = function () {
 
     mostrarComponente("divResumen");
     ocultarComponente("divRol");
